435c7d6a0a42324b5c01616e0755fd1d090abcfd
juanprietob <juanprietob@gmail.com>
2016-07-11 12:54:54 -0400

    ENH: Add packages dcbi-model to the package.json file
    The packages are now independent. The index.js file is modified to load these packages.

--- index.js
+++ index.js
@@ -1,34 +1,107 @@
+var Hapi = require('hapi');
+var fs = require('fs');
+var good = require('good');
+var path = require('path');
 
-var clusterpostserver = require('clusterpost-server');
+var env = process.env.NODE_ENV;
 
-var plugins = [];
-
-var dcbiaModel = {};
-dcbiaModel.register = require('dcbia-model');
-dcbiaModel.options = {
-	"default": "clusterjobs",
-	"clusterjobs": {
-		"hostname": "http://localhost:5984",
-		"database": "clusterjobs"
-	}
-};
+if(!env) throw "Please set NODE_ENV variable.";
 
 
-var dcbiaOrtho = {};
-dcbiaOrtho.register = require('dcbia-ortholab');
-dcbiaOrtho.options = {}
+const getConfigFile = function () {
+  try {
+    // Try to load the user's personal configuration file
+    return require(process.cwd() + '/conf.my.' + env + '.json');
+  } catch (e) {
+    // Else, read the default configuration file
+    return require(process.cwd() + '/conf.' + env + '.json');
+  }
+}
 
-plugins.push(dcbiaModel);
-plugins.push(dcbiaOrtho);
+const startServer = function(cluster){
 
+    var conf = getConfigFile();
+    
+    var server = new Hapi.Server();
 
-clusterpostserver.server.register(plugins, function(err){
-    if (err) {
-        throw err; // something bad happened loading the plugin
+    if(conf.tls && conf.tls.key && conf.tls.cert){
+        const tls = {
+          key: fs.readFileSync(conf.tls.key),
+          cert: fs.readFileSync(conf.tls.cert)
+        };
     }
 
-    clusterpostserver.server.start(function () {
-	   clusterpostserver.server.log('info', 'Server running at: ' + clusterpostserver.server.info.uri);
-	});
+    server.connection({ 
+        host: conf.host,
+        port: conf.port,
+        tls: tls
+    });
+
+    var plugins = [];
+
+    Object.keys(conf.plugins).forEach(function(pluginName){
+        var plugin = {};
+        plugin.register = require(pluginName);
+        plugin.options = conf.plugins[pluginName];
+        plugins.push(plugin);
+    });
+
+    plugins.push({
+        register: good,
+        options: {
+            reporters: [
+            {
+                reporter: require('good-console'),
+                events: { log: '*', response: '*' }
+            }, {
+                reporter: require('good-file'),
+                events: { ops: '*' },
+                config: 'all.log'
+            }]
+        }
+    });
+
+    if(cluster){
+        server.method({
+            name: 'cluster.getWorker',
+            method: function(){
+                return cluster.worker;
+            },
+            options: {}
+        });
+    }
+
+    server.register(plugins, function(err){
+        if (err) {
+            throw err; // something bad happened loading the plugin
+        }
+
+    });
     
-});
+    server.start(function () {
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+}
+
+if(env === 'production'){
+    const cluster = require('cluster');
+    const numCPUs = require('os').cpus().length;
+
+    if (cluster.isMaster) {
+      // Fork workers.
+      for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+      }
+
+      cluster.on('exit', (worker, code, signal) => {
+        console.log("worker ", worker.process.pid,"died");
+      });
+      
+    } else {
+        startServer(cluster);
+    }
+}else{
+
+    startServer();
+}
+
