4302702c74fcb8e7674de6e0304cfb50ef577353
juanprietob <juanprietob@gmail.com>
2016-09-02 13:24:19 -0400

    ENH: Addition of final TMJ form with both questionnaires
    This form merges both questionnaires and fixes Joint Sounds question

---
+++ src/dcbia-ortholab/DCBIA-OrthoLab/scripts/directives/directiveTMJSurvey.js
@@ -0,0 +1,32 @@
+angular.module('cTRIVIAL')
+.directive('tmjSurvey', function($rootScope, $location, $anchorScroll) {
+
+	function link($scope){
+		
+		if(!$scope.formData || ($scope.formData && $scope.formData.formId !== 'TMJSurvey')){
+			$scope.formData = {};
+			$scope.formData.formId = 'TMJSurvey';
+			$scope.formData.date = new Date();
+		}
+
+		$scope.goToScroll = function(location) {
+	      // set the location.hash to the id of
+	      // the element you wish to scroll to.
+	      $location.hash(location);
+
+	      // call $anchorScroll()
+	      $anchorScroll();
+	    };
+	}
+	return {
+    	restrict : 'E',
+    	link : link,
+    	scope: {
+	    	formData : "=",
+	    	editFields: "="
+	    },
+    	templateUrl: 'views/directives/directiveTMJSurvey.html'
+	}
+
+
+});
