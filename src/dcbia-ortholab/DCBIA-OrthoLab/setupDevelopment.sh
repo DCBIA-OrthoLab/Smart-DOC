a2c8105bedd22c6ada4f9ccd21fc4a91152e4d43
juanprietob <juanprietob@gmail.com>
2016-09-14 14:55:13 -0400

    ENH: Add scrpt to setup the project for development

---
+++ src/dcbia-ortholab/DCBIA-OrthoLab/setupDevelopment.sh
@@ -0,0 +1,20 @@
+linkModule(){
+	module=$1
+
+	cd $module
+	bower link
+	modname=$(node -e "var fs = require('fs'); var a = JSON.parse(fs.readFileSync('bower.json')); console.log(a.name); process.exit('success')")
+	cd ..
+	bower link $modname 
+}
+
+linkModule boxPlot
+linkModule circlePlot
+linkModule clinicalData
+linkModule d3Plots
+linkModule dcbia-surveys
+linkModule home
+linkModule jwtUserLogin
+linkModule morphologicalData
+linkModule navBar
+linkModule usersManager
