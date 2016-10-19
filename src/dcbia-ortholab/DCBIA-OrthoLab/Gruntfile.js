96bcf1bde5e38f4ced7f14348a1101f503cf349a
juanprietob <juanprietob@gmail.com>
2016-10-19 12:41:47 -0400

    ENH: Add grunt file for build mechanism of different packages
    The grunt dev options allows debugging the application

---
+++ src/dcbia-ortholab/DCBIA-OrthoLab/Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt) {
+    var name = 'clusterpost-list';
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        run_grunt: {
+            prod: {
+                options: {
+                    log: false,
+                    process: function(res){
+                        if (res.fail){
+                            console.error(res);
+                        }
+                    }
+                },
+                src: ['./data-collections/Gruntfile.js',
+                './dcbia-plots/Gruntfile.js',
+                './jwtUserLogin/Gruntfile.js',
+                './nav-bar/Gruntfile.js']
+            },
+            dev: {
+                options: {
+                    log: true,
+                    process: function(res){
+                        if (res.fail){
+                            console.error(res);
+                        }
+                    },
+                    debugCli: true,
+                    task: ['dev']
+                },
+                src: ['./data-collections/Gruntfile.js',
+                './dcbia-plots/Gruntfile.js',
+                './jwtUserLogin/Gruntfile.js',
+                './nav-bar/Gruntfile.js']
+            },
+        },
+        watch: {
+            options: {
+                livereload: {
+                  port: 9000
+                }
+            },
+            html: {
+                files: ['./data-collections/src/*.html',
+                './dcbia-plots/src/*.html',
+                './jwtUserLogin/src/*.html',
+                './nav-bar/src/*.html'],
+                tasks: ['run_grunt:dev']
+            },
+            css: {
+                files: ['./data-collections/src/*.css',
+                './dcbia-plots/src/*.css',
+                './jwtUserLogin/src/*.css',
+                './nav-bar/src/*.css'],
+                tasks: ['run_grunt:dev']
+            },
+            js: {
+                files: ['./data-collections/src/*.js',
+                './dcbia-plots/src/*.js',
+                './jwtUserLogin/src/*.js',
+                './nav-bar/src/*.js'],
+                tasks: ['run_grunt:dev']
+            },
+        }
+    });
+
+    //load grunt tasks
+    grunt.loadNpmTasks('grunt-run-grunt');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+    //register grunt default task
+    grunt.registerTask('default', [ 'run_grunt:prod']);
+    //register dev task
+    grunt.registerTask('dev', [ 'run_grunt:dev', 'watch']);
+}
