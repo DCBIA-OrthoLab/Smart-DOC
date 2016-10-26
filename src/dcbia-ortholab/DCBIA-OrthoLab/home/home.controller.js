df729750ed02fe677b600a9b2112f771894714f9
Clément Mirabel <clement.mirabel@gmail.com>
2016-10-26 16:11:35 -0400

    ENH: Now display week/month/year survey activity.

--- src/dcbia-ortholab/DCBIA-OrthoLab/home/home.controller.js
+++ src/dcbia-ortholab/DCBIA-OrthoLab/home/home.controller.js
@@ -2,43 +2,135 @@
 angular.module('home')
 .controller('home', ['$scope','$http','dcbia','clusterauth', function($scope, $http, dcbia, clusterauth) {
   
-  $scope.pieLabels = [];
-  $scope.pieData = [];  
-  $scope.pieOptions = { 
-    responsive: true,
-    maintainAspectRatio: false,
-    title: {
-        display: true,
-        text: 'Surveys done by user',
-        fontSize: 17
-    },
-    animation:{
-        animateScale:true
-    }
-  }
-
   clusterauth.getUser()
   .then(function(res){
     $scope.user = res;
-  })  
+  })
 
-  dcbia.getClinicalDataOwners()
+  $scope.pieChartData = {
+    labels: [],
+    data: [],
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      title: {
+        display: true,
+        text: 'Surveys done by user',
+        fontSize: 17
+      },
+      animation:{
+        animateScale:true
+      }
+    }
+  };
+
+  $scope.lineChartData = {
+    selectedDates: 1,
+    labels: [],
+    series: ["Surveys done"],
+    data: [[65, 59, 80, 81, 56, 55, 40]],
+    options: {
+      title: {
+        text: "Contribution activity",
+        display: true,
+        fontSize: 17
+      }
+    }
+  };
+
+  // $scope.pieChartData.getDataOwners = function(){
+    dcbia.getClinicalDataOwners()
+      .then(function(res){
+        $scope.ownersData = res.data;
+        _.each($scope.ownersData,function(owner){
+          index = $scope.pieChartData.labels.indexOf(owner.owner);
+          if( index == -1) {
+            $scope.pieChartData.labels.push(owner.owner);
+            $scope.pieChartData.data.push(1);
+          }
+          else{
+            $scope.pieChartData.data[index]+=1;
+          }
+        })
+    })
+  // }
+
+  var date = new Date();
+  var monthNames = [
+    "Jan", "Feb", "Mar",
+    "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep",
+    "Oct", "Nov", "Dec"
+  ];
+
+  $scope.lineChartData.showOneWeek = function(){
+    $scope.lineChartData.selectedDates = 1;
+    $scope.lineChartData.labels = [];
+    $scope.lineChartData.data = [[]];
+    for (var i = 0; i < 7; i++){
+      var tempDate = new Date();
+      tempDate.setDate(date.getDate()-(6-i));
+      var str = monthNames[tempDate.getMonth()] + " " + tempDate.getDate();
+      $scope.lineChartData.labels.push(str);
+      $scope.lineChartData.data[0].push(0);
+    }
+    dcbia.getClinicalDataOwner($scope.user.email)
     .then(function(res){
-      $scope.ownersData = res.data;
-      _.each($scope.ownersData,function(owner){
-        index = $scope.pieLabels.indexOf(owner.owner);
-        if( index == -1) {
-          $scope.pieLabels.push(owner.owner);
-          $scope.pieData.push(1);
-        }
-        else{
-          $scope.pieData[index]+=1;
+      _.each(res.data,function(survey){
+        var date = monthNames[parseInt(survey.date.toString().substr(5,2))-1] + " " + survey.date.toString().substr(8,2);
+        index = $scope.lineChartData.labels.indexOf(date);
+        if( index != -1) {
+          $scope.lineChartData.data[0][index]+=1;
         }
       })
-  })
-    
+    })
+  };
 
+  $scope.lineChartData.showOneMonth = function(){
+    $scope.lineChartData.selectedDates = 2;
+    $scope.lineChartData.labels = [];
+    $scope.lineChartData.data = [[]];
+    for (var i = 0; i < 31; i++){
+      var tempDate = new Date();
+      tempDate.setDate(date.getDate()-(30-i));
+      var str = monthNames[tempDate.getMonth()] + " " + tempDate.getDate();
+      $scope.lineChartData.labels.push(str); 
+      $scope.lineChartData.data[0].push(0); 
+    }
+    dcbia.getClinicalDataOwner($scope.user.email)
+    .then(function(res){
+      _.each(res.data,function(survey){
+        var date = monthNames[parseInt(survey.date.toString().substr(5,2))-1] + " " + survey.date.toString().substr(8,2);
+        index = $scope.lineChartData.labels.indexOf(date);
+        if( index != -1) {
+          $scope.lineChartData.data[0][index]+=1;
+        }
+      })
+    })
+  };
 
+  $scope.lineChartData.showOneYear = function(){
+    $scope.lineChartData.selectedDates = 3;
+    $scope.lineChartData.labels = [];
+    $scope.lineChartData.data = [[]];
+    for (var i = 0; i < 12; i++){
+      var tempDate = new Date();
+      tempDate.setDate(date.getDate()-(31*(11-i)));
+      var str = monthNames[tempDate.getMonth()] + " " + tempDate.getFullYear().toString().substr(2,2);
+      $scope.lineChartData.labels.push(str);  
+      $scope.lineChartData.data[0].push(0);
+    }
+    dcbia.getClinicalDataOwner($scope.user.email)
+    .then(function(res){
+      _.each(res.data,function(survey){
+        var date = monthNames[parseInt(survey.date.toString().substr(5,2))-1] + " " + survey.date.toString().substr(2,2);
+        index = $scope.lineChartData.labels.indexOf(date);
+        if( index != -1) {
+          $scope.lineChartData.data[0][index]+=1;
+        }
+      })
+    })
+  };
 
 }]);
 
