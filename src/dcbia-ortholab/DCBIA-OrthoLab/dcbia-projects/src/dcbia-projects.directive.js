e7b30d68a764895606c9c4cd0678a234268fc824
Clément Mirabel <clement.mirabel@gmail.com>
2016-11-10 14:32:41 -0500

    ENH: Creating a project is now possible.

--- src/dcbia-ortholab/DCBIA-OrthoLab/dcbia-projects/src/dcbia-projects.directive.js
+++ src/dcbia-ortholab/DCBIA-OrthoLab/dcbia-projects/src/dcbia-projects.directive.js
@@ -10,21 +10,28 @@ angular.module('dcbia-projects')
 
 		$scope.projects = {
 			newProject: {
-				items: [],
+				collections: [],
 				type: "project",
-				name: ""
+				name: "",
+				scope: "",
+				description: ""
 			},
 			projects: [],
-			projectsProperties: {
-				"defaultProject": {
-					class: ''
-				}
-			},
 			section: -1
 		};
 
-		$scope.chat = 1;
+		$scope.morphologicalDataCollection = {
+			collections: [],
+			selectedCollections: []
+		};
+		$scope.morphologicalCheckBox = false;
 
+		$scope.clinicalDataCollection = {
+			collections: [],
+			selectedCollections: []
+		};
+
+		$scope.clinicalCheckBox = false;
 
 		$scope.projects.getProjects = function(){
 			return dcbia.getProjects()
@@ -38,16 +45,94 @@ angular.module('dcbia-projects')
 				});
 			})
 			.catch(console.error);
-		}
+		};
+
+		$scope.projects.create = function(newProject){
+			_.each($scope.clinicalDataCollection.selectedCollections, function(collection){
+				_.each($scope.clinicalDataCollection.collections, function(items){
+					if(collection === items.name){
+						$scope.projects.newProject.collections.push({"_id": items._id})
+					}
+				});
+			});
+			_.each($scope.morphologicalDataCollection.selectedCollections, function(collection){
+				_.each($scope.morphologicalDataCollection.collections, function(items){
+					if(collection === items.name){
+						$scope.projects.newProject.collections.push({"_id": items._id})
+					}
+				});
+			});
+			if($scope.projects.newProject.scope === ""){
+				delete $scope.projects.newProject.scope;
+			}
+			dcbia.createProject(newProject)
+			.then(function(res){
+				return $scope.projects.getProjects();
+			})
+			.catch(console.error);
+		};
+
+		$scope.projects.clearForm = function(force){
+			if(!force){
+				force = confirm("Do you want to clear the current changes?");
+			}
+			if(force){
+				$scope.projects.newProject = {
+					collections: [],
+					type: "project",
+					name: "",
+					scope: "",
+					description: ""
+				};
+				$scope.morphologicalDataCollection.selectedCollections = [];
+				$scope.clinicalDataCollection.selectedCollections = [];
+			}
+		};
+
+		$scope.morphologicalDataCollection.getMorphologicalDataCollections = function(){
+			return dcbia.getMorphologicalDataCollections()
+			.then(function(res){
+				$scope.morphologicalDataCollection.collections = res.data;
+			})
+			.catch(console.error);
+		};
+
+		$scope.morphologicalDataCollection.checkAll = function(checkbox) {
+			$scope.morphologicalDataCollection.selectedCollections = [];
+			if(checkbox){
+				_.each($scope.morphologicalDataCollection.collections,function(collection){
+					$scope.morphologicalDataCollection.selectedCollections.push(collection.name);
+            	});
+			}
+  		};
+
+		$scope.clinicalDataCollection.getClinicalDataCollections = function(){
+			return dcbia.getClinicalDataCollections()
+			.then(function(res){
+				$scope.clinicalDataCollection.collections = res.data;
+			})
+			.catch(console.error);
+		};
+
+		$scope.clinicalDataCollection.checkAll = function(checkbox) {
+			$scope.clinicalDataCollection.selectedCollections = [];
+			if(checkbox){
+				_.each($scope.clinicalDataCollection.collections,function(collection){
+					$scope.clinicalDataCollection.selectedCollections.push(collection.name);
+            	});
+			}
+  		};
 		
 		$scope.projects.getProjects();
+		$scope.morphologicalDataCollection.getMorphologicalDataCollections();
+		$scope.clinicalDataCollection.getClinicalDataCollections();
 
 	}
 
 	return {
 		restrict : 'E',
 	    link : link,
-	    templateUrl: 'node_modules/dcbia-projects/src/dcbia-projects.template.html'
+	    templateUrl: './src/dcbia-projects.template.html'
 	}	
 
 });
