0d4b647f3e760dfe9f942adad1d9c46b0ac7947f
juanprietob <juanprietob@gmail.com>
2017-01-15 00:19:51 -0500

    STYLE: Image for documentation

---
+++ doc/sharingDataArticle/convert.sh
@@ -0,0 +1,6 @@
+
+for files in $(ls images/temp/*.png)
+do
+	outname=$(basename ${files%%.png}Out.eps)
+	convert $files images/$outname
+done
--- doc/sharingDataArticle/images/convert.sh
+++
@@ -1,6 +0,0 @@
-dir=$1
-for files in $(ls $dir/*.png)
-do
-	outname=$(basename ${files%%.png}Out.eps)
-	convert $files $outname
-done
