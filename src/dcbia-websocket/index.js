01c8eaa8972cd4fc155680bc2f1d809d25460bc1
Clément Mirabel <clement.mirabel@gmail.com>
2017-03-08 15:07:13 -0500

    ENH: Added Hello_World code for websocket.

--- src/dcbia-websocket/index.js
+++ src/dcbia-websocket/index.js
@@ -1,18 +1,10 @@
 exports.register = function (server, options, next) {
-    require('websocket');
-    
-    // Port where we'll run the websocket server
     var webSocketsServerPort = 1337;
+    var webSocketServer = require('websocket').server;
+    var http = require('http');
 
-    // websocket and http servers
-    // var webSocketServer = server;
-    // var http = require('http');
-
-    /*
-    * Global variables
-    */
-    var history = [];
-    var clients = [];
+    var history = [ ];
+    var clients = [ ];
 
     /**
      * Helper function for escaping input strings
@@ -22,76 +14,77 @@ exports.register = function (server, options, next) {
                           .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
     }
 
+    // Array with some colors
+    var colors = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
+    // ... in random order
+    colors.sort(function(a,b) { return Math.random() > 0.5; } );
+
+    /**
+     * HTTP server
+     */
+    // var server = http.createServer(function(request, response) {
+    //     // Not important for us. We're writing WebSocket server, not HTTP server
+    // });
     const srv = server.connection({
         port: webSocketsServerPort,
         host: 'localhost',
         labels: 'socket'
     });
 
-    srv.route({
-        path: '/',
-        method: 'GET',
-        handler: function (request, reply) {
-            console.log(request)
-            console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
-            // var connection = request.accept(null, "test");
-            // var index = clients.push(connection) - 1;
-            // console.log((new Date()) + ' Connection accepted.');
+    console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
 
-            // user sent some messages
-            // connection.on('message', function(message) {
-    //         if (message.type === 'utf8') { // accept only text
-    //             if (userName === false) { // first message sent by user is their name
-    //                 // remember user name
-    //                 userName = htmlEntities(message.utf8Data);
-    //                 // get random color and send it back to the user
-    //                 userColor = colors.shift();
-    //                 connection.sendUTF(JSON.stringify({ type:'color', data: userColor }));
-    //                 console.log((new Date()) + ' User is known as: ' + userName
-    //                             + ' with ' + userColor + ' color.');
-
-    //             } else { // log and broadcast the message
-    //                 console.log((new Date()) + ' Received Message from '
-    //                             + userName + ': ' + message.utf8Data);
-
-    //                 // we want to keep history of all sent messages
-    //                 var obj = {
-    //                     time: (new Date()).getTime(),
-    //                     text: htmlEntities(message.utf8Data),
-    //                     author: userName,
-    //                     color: userColor
-    //                 };
-    //                 history.push(obj);
-    //                 history = history.slice(-100);
-
-    //                 // broadcast message to all connected clients
-    //                 var json = JSON.stringify({ type:'message', data: obj });
-    //                 for (var i=0; i < clients.length; i++) {
-    //                     clients[i].sendUTF(json);
-    //                 }
-    //             }
-    //         }
-    //     });
-
-    //     // user disconnected
-    //     connection.on('close', function(connection) {
-    //         if (userName !== false && userColor !== false) {
-    //             console.log((new Date()) + " Peer "
-    //                 + connection.remoteAddress + " disconnected.");
-    //             // remove user from the list of connected clients
-    //             clients.splice(index, 1);
-    //             // push back user's color to be reused by another user
-    //             colors.push(userColor);
-    //         }
-    //     });
-
-    // });
-            return reply('hello');
-        }
+    var wsServer = new webSocketServer({
+        httpServer: srv
     });
 
-    console.log((new Date()) + " WebSocket is listening on port " + webSocketsServerPort);
-    
+    wsServer.on('request', function(request) {
+        console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+        var connection = request.accept(null, request.origin);
+        var index = clients.push(connection) - 1;
+        var userName = false;
+        var userColor = false;
+        console.log((new Date()) + ' Connection accepted.');
+        if (history.length > 0) {
+            connection.sendUTF(JSON.stringify( { type: 'history', data: history} ));
+        }
+        connection.on('message', function(message) {
+            if (message.type === 'utf8') {
+                if (userName === false) {
+                    userName = htmlEntities(message.utf8Data);
+                    userColor = colors.shift();
+                    connection.sendUTF(JSON.stringify({ type:'color', data: userColor }));
+                    console.log((new Date()) + ' User is known as: ' + userName
+                        + ' with ' + userColor + ' color.');
+
+                } else {
+                    console.log((new Date()) + ' Received Message from '
+                        + userName + ': ' + message.utf8Data);
+                    var obj = {
+                        time: (new Date()).getTime(),
+                        text: htmlEntities(message.utf8Data),
+                        author: userName,
+                        color: userColor
+                    };
+                    history.push(obj);
+                    history = history.slice(-100);
+
+                    var json = JSON.stringify({ type:'message', data: obj });
+                    for (var i=0; i < clients.length; i++) {
+                        clients[i].sendUTF(json);
+                    }
+                }
+            }
+        });
+
+        connection.on('close', function(connection) {
+            if (userName !== false && userColor !== false) {
+                console.log((new Date()) + " Peer "
+                    + connection.remoteAddress + " disconnected.");
+                clients.splice(index, 1);
+                colors.push(userColor);
+            }
+        });
+    });
 
     return next();
 };
